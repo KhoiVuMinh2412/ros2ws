2355e0c676d13ced3c9812045c5e562b2d2054eb
KhoiVuMinh2412 <khoi.vuminh241207@hcmut.edu.vn>
2026-01-16 22:12:44 +0700

    delete the wrong build files in the source

--- src/build/robocon_interfaces/robocon_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyrgios/ros2ws/src/build/robocon_interfaces/robocon_interfaces__py
-# Build directory: /home/kyrgios/ros2ws/src/build/robocon_interfaces/robocon_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
